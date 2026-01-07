49aa30414714198937315e309815dd27288853c2
Jovany Leandro G.C <bit4bit@riseup.net>
2026-01-07 21:26:53 +0200

    Update compat.ex

--- lib/clj_compiler/compat.ex
+++ lib/clj_compiler/compat.ex
@@ -1,5 +1,7 @@
 defmodule CljCompiler.Compat do
-  @runtime_functions ~w(conj get assoc dissoc assoc_in)
+  @moduledoc """
+  This module provides compatibility functions inspired by the Clojure standard library.
+  """
 
   defmacro __using__(_opts \\ []) do
     quote do
@@ -7,51 +9,27 @@ defmodule CljCompiler.Compat do
     end
   end
 
-  def runtime_functions, do: @runtime_functions
-
-  def call_with_fallback(parent_module, function, args) do
-    arity = length(args)
-
-    if function_exported?(parent_module, function, arity) do
-      apply(parent_module, function, args)
-    else
-      apply(Kernel, function, args)
-    end
+  def conj(collection, item) do
+    [item | collection]
   end
 
-  defmacro conj(collection, item) do
-    quote do
-      [unquote(item) | unquote(collection)]
-    end
+  def get(map, key) do
+    Map.get(map, key)
   end
 
-  defmacro get(map, key) do
-    quote do
-      Map.get(unquote(map), unquote(key))
-    end
+  def get(map, key, default) do
+    Map.get(map, key, default)
   end
 
-  defmacro get(map, key, default) do
-    quote do
-      Map.get(unquote(map), unquote(key), unquote(default))
-    end
+  def assoc(map, key, value) do
+    Map.put(map, key, value)
   end
 
-  defmacro assoc(map, key, value) do
-    quote do
-      Map.put(unquote(map), unquote(key), unquote(value))
-    end
+  def dissoc(map, keys) do
+    Enum.reduce(keys, map, fn key, acc -> Map.delete(acc, key) end)
   end
 
-  defmacro dissoc(map, keys) do
-    quote do
-      Enum.reduce(unquote(keys), unquote(map), fn key, acc -> Map.delete(acc, key) end)
-    end
-  end
-
-  defmacro assoc_in(map, keys, value) do
-    quote do
-      put_in(unquote(map), unquote(keys), unquote(value))
-    end
+  def assoc_in(map, keys, value) do
+    put_in(map, keys, value)
   end
 end
