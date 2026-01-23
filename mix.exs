ddda6869d2d168e01c46ce1e0a248124eeb7751c
Jovany Leandro G.C <bit4bit@riseup.net>
2026-01-23 20:25:29 +0200

    mix warnings as errors

--- mix.exs
+++ mix.exs
@@ -10,7 +10,10 @@ defmodule CljCompiler.MixProject do
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      elixirc_options: [
+        warnings_as_errors: true
+      ]
     ]
   end
 
